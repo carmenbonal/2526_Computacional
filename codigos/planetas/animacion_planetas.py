92ac15de4c8d708fa01b2f06086e38faf96df879
Carmen Bonal <e.carmenbonal@go.ugr.es>
2026-04-08 13:03:30 +0200

    subo la primera version del código de los planetas

--- codigos/planetas/animacion_planetas.py
+++ codigos/planetas/animacion_planetas.py
@@ -51,8 +51,8 @@ file_in = "planets_data.dat" # Nombre del fichero de datos
 file_out = "planetas" # Nombre del fichero de salida (sin extensión)
 
 # Límites de los ejes X e Y
-x_min = -50
-x_max = 50
+x_min = -6
+x_max = 6
 y_min = -50 
 y_max = 50
 
@@ -66,7 +66,7 @@ dpi = 150 # Calidad del vídeo de salida (dots per inch)
 # Radio del planeta, en las mismas unidades que la posición
 # Puede ser un número (el radio de todos los planetas) o una lista con
 # el radio de cada uno
-planet_radius = 1 
+planet_radius = 0.1 
 #planet_radius = [0.5, 0.7, 1.1]
 
 
